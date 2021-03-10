04be45495bb56f925aef855b57629c410f5af8b4
Sabri0o <sabritrabelsi90@gmail.com>
2021-03-10 09:52:02 +0100

    firebase config

--- project/src/firebase.js
+++ project/src/firebase.js
@@ -1,17 +1,18 @@
 // firebase.js configures information to connect with Firebase Project and export Firebase Database service.
+require("dotenv").config();
 
 import * as firebase from "firebase";
 import "firebase/database";
 
 let config = {
-  apiKey: "AIzaSyCHuA3VIkKJ7eEYNB9KmpoGsV4vlYe6xJ8",
+  apiKey: process.env.apiKey,
   authDomain: "react-firebase-crud-71ae5.firebaseapp.com",
   databaseURL:
     "https://react-firebase-crud-71ae5-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "react-firebase-crud-71ae5",
   storageBucket: "react-firebase-crud-71ae5.appspot.com",
-  messagingSenderId: "759839378430",
-  appId: "1:759839378430:web:40467b4c5a1d442d4f8eaf",
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
   measurementId: "G-KYXJL0Z2XD",
 };
 
