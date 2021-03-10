c2f399278865837a3d7040c54f920c9ab6d92cfe
Sabri0o <sabritrabelsi90@gmail.com>
2021-03-10 09:41:31 +0100

    integrating firebase

--- project/src/firebase.js
+++ project/src/firebase.js
@@ -1,1 +1,20 @@
 // firebase.js configures information to connect with Firebase Project and export Firebase Database service.
+
+import * as firebase from "firebase";
+import "firebase/database";
+
+let config = {
+  apiKey: "AIzaSyCHuA3VIkKJ7eEYNB9KmpoGsV4vlYe6xJ8",
+  authDomain: "react-firebase-crud-71ae5.firebaseapp.com",
+  databaseURL:
+    "https://react-firebase-crud-71ae5-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "react-firebase-crud-71ae5",
+  storageBucket: "react-firebase-crud-71ae5.appspot.com",
+  messagingSenderId: "759839378430",
+  appId: "1:759839378430:web:40467b4c5a1d442d4f8eaf",
+  measurementId: "G-KYXJL0Z2XD",
+};
+
+firebase.initializeApp(config);
+
+export default firebase.database();
--- project/src/services/itemService.js
+++ project/src/services/itemService.js
@@ -4,3 +4,31 @@
 // addItem for creating new item
 // Itemlist contains list of items
 // item shows item details
+
+import firebase from "../firebase";
+
+const db = firebase.ref("/items");
+
+class ItemDataService {
+  getAll() {
+    return db;
+  }
+
+  create(item) {
+    return db.push(item);
+  }
+
+  update(key, value) {
+    return db.child(key).update(value);
+  }
+
+  delete(key) {
+    return db.child(key).remove();
+  }
+
+  deleteAll() {
+    return db.remove();
+  }
+}
+
+export default new ItemDataService();
