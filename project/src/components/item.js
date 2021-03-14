07c0ae61a30ec4f931f9f864f9f907568818c916
Sabri0o <sabritrabelsi90@gmail.com>
2021-03-14 17:19:51 +0100

    update functionality

--- project/src/components/item.js
+++ project/src/components/item.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ItemDataService from '../services/itemService'
 export default class item extends Component {
   constructor(props) {
     super(props);
@@ -8,9 +9,117 @@ export default class item extends Component {
         title: "",
         description: "",
       },
+      message :''
+
     };
     this.handleOnChange = this.handleOnChange.bind(this);
+    this.updateItem = this.updateItem.bind(this)
   }
+  componentWillReceiveProps(nextProps) {
+    console.log("Receiving new props...");
+  }
+  componentDidUpdate() {
+    console.log("Component re-rendered.");
+  }
+  
+  componentDidMount() {
+    this.setState({
+      currentItem: this.props.selected,
+    });
+  }
+  
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log("prevState: ", prevState);
+    console.log("nextProps: ", nextProps);
+    const { selected } = nextProps;
+    if (prevState.currentItem.key !== selected.key) {
+      return {
+        currentItem: selected,
+      };
+    }
+    
+    return prevState.currentItem;
+  }
+  
+  handleOnChange(e) {
+    this.setState((state) => ({
+      currentItem: {
+        ...state.currentItem,
+        [e.target.name]: e.target.value,
+      },
+    }));
+  }
+  
+  updateItem() {
+    const data = {
+      title: this.state.currentItem.title,
+      description: this.state.currentItem.description,
+    };
+    
+    ItemDataService.update(this.state.currentItem.key, data)
+    .then(() => {
+      this.setState({
+        currentItem: {
+          key: null,
+          title: "",
+          description: "",
+        },
+        message : 'Item was updated successfully !'
+      });
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+  }
+  
+  render() {
+    // console.log('currentItem: ',currentItem)
+    return (
+      <div className="edit-form">
+        <form>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              value={this.state.currentItem.title}
+              name="title"
+              onChange={this.handleOnChange}
+              />
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <input
+              type="text"
+              className="form-control"
+              id="description"
+              value={this.state.currentItem.description}
+              name="description"
+              onChange={this.handleOnChange}
+              />
+          </div>
+        </form>
+
+        <button
+          className="badge badge-danger mr-2"
+          >
+          Delete
+        </button>
+
+        <button
+          type="submit"
+          className="badge badge-success"
+          onClick={this.updateItem}
+          >
+          Update
+        </button>
+        <p>{this.state.message}</p>
+      </div>
+    );
+  }
+}
+
 
   // shouldComponentUpdate(nextProps, prevState) {
   //   console.log('Should I update?');
@@ -29,85 +138,4 @@ export default class item extends Component {
   //     return false
   //     }
   // }
-  componentWillReceiveProps(nextProps) {
-    console.log("Receiving new props...");
-  }
-  componentDidUpdate() {
-    console.log("Component re-rendered.");
-  }
-
-  componentDidMount() {
-    this.setState({
-      currentItem: this.props.selected,
-    });
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    console.log("prevState: ", prevState);
-    console.log("nextProps: ", nextProps);
-    const { selected } = nextProps;
-    if (prevState.currentItem.key !== selected.key) {
-      return {
-        currentItem: selected,
-      };
-    }
-
-    return prevState.currentItem;
-  }
-
-  handleOnChange(e) {
-    this.setState((state) => ({
-      currentItem: {
-        ...state.currentItem,
-        [e.target.name]: e.target.value,
-      },
-    }));
-  }
-
-  render() {
-    // console.log('currentItem: ',currentItem)
-    return (
-      <div className="edit-form">
-        <form>
-          <div className="form-group">
-            <label htmlFor="title">Title</label>
-            <input
-              type="text"
-              className="form-control"
-              id="title"
-              value={this.state.currentItem.title}
-              name="title"
-              onChange={this.handleOnChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="description">Description</label>
-            <input
-              type="text"
-              className="form-control"
-              id="description"
-              value={this.state.currentItem.description}
-              name="description"
-              onChange={this.handleOnChange}
-            />
-          </div>
-        </form>
-
-        <button
-          className="badge badge-danger mr-2"
-          onClick={this.deleteTutorial}
-        >
-          Delete
-        </button>
-
-        <button
-          type="submit"
-          className="badge badge-success"
-          onClick={this.updateTutorial}
-        >
-          Update
-        </button>
-      </div>
-    );
-  }
-}
+  
